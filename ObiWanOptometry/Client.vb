4cb611f2e145cb8d2b4c9623b32e8272975e2ad0
tizz98 <dev.tizz98@gmail.com>
2016-01-22 09:45:37 -0800

    Client docs

--- ObiWanOptometry/Client.vb
+++ ObiWanOptometry/Client.vb
@@ -1,4 +1,15 @@
-﻿Public Class Client
+﻿'------------------------------------------------------------
+'-                   File Name: Client.vb                   -
+'-                 Part of Project: Assign2                 -
+'------------------------------------------------------------
+'-                Written By: Elijah Wilson                 -
+'-                  Written On: 01/22/2016                  -
+'------------------------------------------------------------
+'- File Purpose:                                            -
+'-                                                          -
+'- This file contains the Client class.                     -
+'------------------------------------------------------------
+Public Class Client
     Inherits ReceiptFormatter
     Implements ReceiptItem
 
@@ -22,10 +33,53 @@
     Private Const RECEIPT_TOTAL_STR As String = "Total"
     Private Const SALES_TAX_AMOUNT As Decimal = 0.06  ' 6% sales tax
 
+    '------------------------------------------------------------
+    '-                 Function Name: validate                  -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/22/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- This function returns true or false based on whether or  -
+    '- not the client has valid data.                           -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- Boolean - Whether or not the client object is valid      -
+    '------------------------------------------------------------
     Public Function validate() As Boolean Implements ReceiptItem.validate
         Return validateGlasses() And validateContacts()
     End Function
 
+    '------------------------------------------------------------
+    '-              Function Name: validateGlasses              -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/22/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- This function returns  true or false based on whether or -
+    '- not the glasses object of the client has valid data.     -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- isValid - A boolean that will be returned, holding the   -
+    '-           value of the glasses object being valid or     -
+    '-           not.                                           -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- Boolean - Whether or not the glasses object of a client  -
+    '-           is valid                                       -
+    '------------------------------------------------------------
     Private Function validateGlasses() As Boolean
         Dim isValid As Boolean = False
 
@@ -38,6 +92,28 @@
         Return isValid
     End Function
 
+    '------------------------------------------------------------
+    '-             Function Name: validateContacts              -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/22/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- This function returns  true or false based on whether or -
+    '- not the contacts object of the client has valid data.    -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- isValid - A boolean that will be returned, holding the   -
+    '-           value of the contacts being valid or not.      -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- Boolean - Whether or not the contacts object of a client -
+    '-           is valid                                       -
+    '------------------------------------------------------------
     Private Function validateContacts() As Boolean
         Dim isValid As Boolean = False
 
@@ -50,6 +126,26 @@
         Return isValid
     End Function
 
+    '------------------------------------------------------------
+    '-                Function Name: getSubtotal                -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/22/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- This returns the subtotal based on what services are     -
+    '- selected.                                                -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- subtotal - The accruing subtotal to be returned          -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- Decimal - The subtotal for the client object             -
+    '------------------------------------------------------------
     Private Function getSubtotal() As Decimal Implements ReceiptItem.getSubtotal
         Dim subtotal As Decimal = 0.0
 
@@ -68,26 +164,148 @@
         Return subtotal
     End Function
 
+    '------------------------------------------------------------
+    '-          Function Name: getSubtotalReceiptLine           -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/22/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- This returns the subtotal line for the receipt.          -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- String - The subtotal receipt line for the client object -
+    '------------------------------------------------------------
     Private Function getSubtotalReceiptLine() As String
         Return getSubItemLineFormatted(RECEIPT_SUBTOTAL_STR, getSubtotal())
     End Function
 
+    '------------------------------------------------------------
+    '-             Function Name: getSalesTaxAmount             -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/22/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- This returns how much sales tax should be added to the   -
+    '- total based on the subtotal and the sales tax amount.    -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- Decimal - How much sales tax should be added to the      -
+    '-           total                                          -
+    '------------------------------------------------------------
     Private Function getSalesTaxAmount() As Decimal
         Return getSubtotal() * SALES_TAX_AMOUNT
     End Function
 
+    '------------------------------------------------------------
+    '-          Function Name: getSalesTaxReceiptLine           -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/22/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- This returns the properly formatted line for sales tax   -
+    '- to be used on the receipt.                               -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- String - Sales tax line for the receipt                  -
+    '------------------------------------------------------------
     Private Function getSalesTaxReceiptLine() As String
         Return getSubItemLineFormatted(SALES_TAX_STR, getSalesTaxAmount())
     End Function
 
+    '------------------------------------------------------------
+    '-           Function Name: getReceiptTotalAmount           -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/22/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- This returns the subtotal plus sales tax, which is the   -
+    '- total amount to be used on the receipt.                  -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- Decimal - Subtotal + Sales tax                           -
+    '------------------------------------------------------------
     Private Function getReceiptTotalAmount() As Decimal
         Return getSubtotal() + getSalesTaxAmount()
     End Function
 
+    '------------------------------------------------------------
+    '-            Function Name: getReceiptTotalLine            -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/22/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- This returns the total cost line for the receipt.        -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- String - Total cost line for receipt                     -
+    '------------------------------------------------------------
     Private Function getReceiptTotalLine() As String
         Return getSubItemLineFormatted(RECEIPT_TOTAL_STR, getReceiptTotalAmount())
     End Function
 
+    '------------------------------------------------------------
+    '-             Function Name: getReceiptOutput              -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/22/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- This returns the subtotal based on what services are     -
+    '- selected.                                                -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- returnString - The string that is used to add data based -
+    '-                on whether or not certain services are    -
+    '-                selected. In the end, it is returned.     -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- String - The string to be used when displaying the       -
+    '-          receipt                                         -
+    '------------------------------------------------------------
     Public Function getReceiptOutput() As String Implements ReceiptItem.getReceiptOutput
         Dim returnString As String = getReceiptHeader(frmClientDetailInput.BASE_TITLE) & String.Format(CLIENT_NAME_FORMAT_STR, name) & vbCrLf & vbCrLf
 
